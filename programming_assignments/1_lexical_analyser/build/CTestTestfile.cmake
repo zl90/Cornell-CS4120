5fa6df2f2e47716a547b8bc66ad103405ad9e6c6
mb541 <mb541@protonmail.com>
2023-10-25 21:48:28 +1100

    Add boilerplate and Gtest

---
+++ programming_assignments/1_lexical_analyser/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mint/Documents/code/Cornell-CS4120/programming_assignments/1_lexical_analyser
+# Build directory: /home/mint/Documents/code/Cornell-CS4120/programming_assignments/1_lexical_analyser/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[add]=] "etac")
+set_tests_properties([=[add]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/mint/Documents/code/Cornell-CS4120/programming_assignments/1_lexical_analyser/CMakeLists.txt;10;add_test;/home/mint/Documents/code/Cornell-CS4120/programming_assignments/1_lexical_analyser/CMakeLists.txt;0;")
