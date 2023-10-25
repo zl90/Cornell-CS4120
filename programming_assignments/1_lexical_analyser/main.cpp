083cff6a94e42ffa6bacf2da3edf02c4f897298f
mb541 <mb541@protonmail.com>
2023-10-25 22:34:39 +1100

    Handle --help CLI argument

--- programming_assignments/1_lexical_analyser/main.cpp
+++ programming_assignments/1_lexical_analyser/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <gtest/gtest.h>
 
 int add(int a, int b)
@@ -7,13 +8,27 @@ int add(int a, int b)
     return a + b;
 }
 
-TEST(add, positive)
+// TEST(add, positive)
+// {
+//     ASSERT_EQ(10, add(5, 5));
+// }
+
+void print_synopsis()
 {
-    ASSERT_EQ(10, add(5, 5));
+    std::cout << "Printing synopsis...\n";
+}
+
+void handle_cli_args(const int length, const char *const args[])
+{
+    if (length == 1 || (length == 2 && std::strcmp(args[1], "--help") == 0))
+    {
+        print_synopsis();
+    }
 }
 
 int main(int argc, char *argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
+    handle_cli_args(argc, argv);
     return RUN_ALL_TESTS();
 }
